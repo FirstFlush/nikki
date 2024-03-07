2a0a8260608c2b2846b0528119817a2492a439ba
FirstFlush <firstflush@protonmail.com>
2024-03-06 19:50:04 -0800

    prepended api to media and static roots in django app

--- nikki_backend/racecar_build/views.py
+++ nikki_backend/racecar_build/views.py
@@ -1,4 +1,5 @@
-from rest_framework.views import Request, Response
+from django.http import HttpRequest
+from rest_framework.views import Response
 from nikki_backend.base_views import NikkiAPIView
 from .models import NikkiImage
 from .serializers import NikkiImageSerializer
@@ -6,10 +7,13 @@ from .serializers import NikkiImageSerializer
 
 class GalleryView(NikkiAPIView):
 
-    def get(self, req:Request, *args, **kwargs):
+    def get(self, req:HttpRequest, *args, **kwargs):
         imgs = NikkiImage.objects.filter(is_active=True)
         context = {
             'req':req,
         }
         serializer = NikkiImageSerializer(instance=imgs, context=context, many=True)
+
+        print(req.build_absolute_uri('asdffdsa'))
+        print('+'*80)
         return Response ({'imgs':serializer.data})
