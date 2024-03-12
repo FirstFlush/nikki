e977f853d8732f9dad98a7f7ecce8085d7938acd
FirstFlush <firstflush@protonmail.com>
2024-03-11 17:21:57 -0700

    fuck this

--- nikki_backend/nikki_backend/settings.py
+++ nikki_backend/nikki_backend/settings.py
@@ -171,10 +171,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'api/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'api/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = 'api/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'api/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
